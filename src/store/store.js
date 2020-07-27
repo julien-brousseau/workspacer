c0cb8c7f434b84b84a4691c08bfc35cfa4ebddd0
JulBrousseau <julien@brousseau.tech>
2020-07-27 14:03:22 -0400

    Re-write promises for consistency

--- src/store/store.js
+++ src/store/store.js
@@ -4,27 +4,6 @@ import Vuex from 'vuex'
 Vue.use(Vuex)
 
 const browser = require('webextension-polyfill')
-// const browser = {}
-// browser.set = new Promise((resolve, reject) => {
-
-// })
-
-// const identities = await browser.contextualIdentities.query({})
-// const identity = identities.filter(i => i.name === "Brousseau.tech")[0].cookieStoreId
-
-// const data = {
-//   ws: {
-//     Coding: [
-//       { title: 'Vue.js App', url: 'localhost:8080', pinned: false, index: 0, discarded: false },
-//       { title: 'Firefox', url: 'firefox.com', pinned: false, index: 1, discarded: true },
-//       { title: 'Github', url: 'github.com', pinned: false, index: 2, discarded: true }
-//     ],
-//     Personal: [
-//       { title: 'Bank', url: 'tangerine.ca', pinned: false, index: 0, discarded: false },
-//       { title: 'Search', url: 'google.ca', pinned: false, index: 1, discarded: false }
-//     ]
-//   } //, cookieStoreId: identity
-// }
 
 export default new Vuex.Store({
   state: {
@@ -35,7 +14,6 @@ export default new Vuex.Store({
 
     'UPDATE_ALL_WS' (state, ws) {
       state.ws = ws
-      console.log('STATE >> ', state.ws)
     },
 
     'TOGGLE_ADDING_WS' (state, visible) {
@@ -47,51 +25,52 @@ export default new Vuex.Store({
 
     // Initially load storage WS
     initWS: async ({ dispatch, commit }) => {
-      const ws = await dispatch('loadWS')
-      commit('UPDATE_ALL_WS', ws)
+      dispatch('loadWS')
+        .then(ws => commit('UPDATE_ALL_WS', ws))
+        .catch(e => console.log('Error: :>> ', e))
     },
 
     // Create a new Workspace object
-    addWS: async ({ dispatch, getters }, { name }) => {
+    addWS: ({ dispatch, getters }, { name }) => {
       const ws = { ...getters.allWS, [name]: [] }
       dispatch('saveWS', ws)
     },
 
     // Create a new tab in the selected Workspace
     addTabToWS: async ({ dispatch, getters }, wsName) => {
-      const currentTab = await browser.tabs.query({ currentWindow: true, active: true })
-      const { id, title, url } = currentTab[0]
-      const ws = { ...getters.allWS }
-      const tab = { id, title, url }
-      ws[wsName].push(tab)
-      dispatch('saveWS', ws)
+      browser.tabs.query({ currentWindow: true, active: true })
+        .then(currentTab => currentTab[0])
+        .then(({ id, title, url }) => {
+          const ws = { ...getters.allWS }
+          const tab = { id, title, url }
+          ws[wsName].push(tab)
+          dispatch('saveWS', ws)
+        })
+        .catch(e => console.log('Error: :>> ', e))
     },
 
     // Save the stringyfied ws object to storage, and commit the returned storage to state
     saveWS: async ({ dispatch, commit }, ws) => {
       const jsonWS = JSON.stringify(ws)
       await browser.storage.local.set({ ws: jsonWS })
-      const storedWS = await dispatch('loadWS')
-      commit('UPDATE_ALL_WS', storedWS)
+      dispatch('loadWS')
+        .then(storedWS => commit('UPDATE_ALL_WS', storedWS))
+        .catch(e => console.log('Error: :>> ', e))
     },
 
     // Returns the JSON parsed content of local storage {ws} variable
     loadWS: async () => {
       return browser.storage.local.get('ws')
-        .then(({ ws }) => {
-          return JSON.parse(ws)
-        })
-        .catch(e => {
-          return { e }
-        })
+        .then(({ ws }) => JSON.parse(ws))
+        .catch(e => console.log('Error: :>> ', e))
     },
 
-    // Reset the ws storage object
-    clearWS: async ({ dispatch }) => {
+    // Reset the ws storage with empty object
+    clearWS: ({ dispatch }) => {
       dispatch('saveWS', {})
     },
 
-    //
+    // Show/hide the New WS form
     toggleAddingWS: ({ commit, state }) => {
       commit('TOGGLE_ADDING_WS', !state.addingWS)
     }
