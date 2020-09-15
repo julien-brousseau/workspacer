6ad7e93e01dbe91587a551cc0ad2d4680be2e78a
JulBrousseau <julien@brousseau.tech>
2020-09-15 17:19:59 -0400

    Change root files hierarchy to implement background scripts support

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   root: true,
   env: {
     node: true,
-    es2020: true
+    es2020: true,
+    webextensions: true
   },
   extends: [
     'plugin:vue/essential',
@@ -14,16 +15,16 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+  }
+  // overrides: [
+  //   {
+  //     files: [
+  //       '**/__tests__/*.{j,t}s?(x)',
+  //       '**/tests/unit/**/*.spec.{j,t}s?(x)'
+  //     ],
+  //     env: {
+  //       jest: true
+  //     }
+  //   }
+  // ]
 }
